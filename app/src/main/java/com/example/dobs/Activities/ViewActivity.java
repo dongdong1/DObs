765f3c713ba466900958f5789232180840a0ce66
Dade <dade@live.cn>
2016-03-03 23:06:26 -0500

    Added ViewActivity

---
+++ app/src/main/java/com/example/dobs/Activities/ViewActivity.java
@@ -0,0 +1,36 @@
+package com.example.dobs.Activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.dobs.R;
+
+public class ViewActivity extends AppCompatActivity {
+    private AppCompatActivity context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        this.context = this;
+
+        Button btnBehavior = (Button) findViewById(R.id.btnBehavior);
+        btnBehavior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, ViewBehaviors.class));
+            }
+        });
+
+        Button btnSleep = (Button) findViewById(R.id.btnSleep);
+        btnSleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, ViewBehaviors.class));
+            }
+        });
+    }
+}
