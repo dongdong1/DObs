c2760a9aa82e6d0fed27323f9004188248150958
Dade <dade@live.cn>
2016-03-06 21:09:25 -0500

    LoadScreen onClick start loading

--- app/src/main/java/com/example/dobs/Fragments/LoadScreen.java
+++ app/src/main/java/com/example/dobs/Fragments/LoadScreen.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.example.dobs.R;
@@ -30,26 +31,32 @@ public class LoadScreen extends Fragment {
         View resultView = inflater.inflate(R.layout.frag_load_screen, container, false);
         progressBar = (ProgressBar) resultView.findViewById(R.id.progressBar);
         handler = new Handler();
-        new Thread(new Runnable() {
-            public void run() {
-                while (progressStatus < 100) {
-                    progressStatus += 1;
-                    // Update the progress bar and display the current value in the text view
-                    handler.post(new Runnable() {
-                        public void run() {
-                            progressBar.setProgress(progressStatus);
+        LinearLayout layout = (LinearLayout) resultView.findViewById(R.id.loadScreen);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressStatus < 100) {
+                            progressStatus += 1;
+                            // Update the progress bar and display the current value in the text view
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressStatus);
+                                }
+                            });
+                            try {
+                                // Sleep for 20 milliseconds
+                                Thread.sleep(20);
+                            } catch (InterruptedException e) {
+                                Log.e(this.getClass().toString(), e.getMessage());
+                            }
                         }
-                    });
-                    try {
-                        // Sleep for 50 milliseconds
-                        Thread.sleep(20);
-                    } catch (InterruptedException e) {
-                        Log.e(this.getClass().toString(), e.getMessage());
+                        startMainFragment();
                     }
-                }
-                startMainFragment();
+                }).start();
             }
-        }).start();
+        });
         return resultView;
     }
 
